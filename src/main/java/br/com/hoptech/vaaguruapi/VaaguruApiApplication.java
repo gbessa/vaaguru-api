0b33d0bb252f57eb3cbdb5c1721ba93477ba3566
Gustavo Bessa <gbvirtual@gmail.com>
2018-06-29 16:10:53 -0300

    First commit

---
+++ src/main/java/br/com/hoptech/vaaguruapi/VaaguruApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.hoptech.vaaguruapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaaguruApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaaguruApiApplication.class, args);
+	}
+}
