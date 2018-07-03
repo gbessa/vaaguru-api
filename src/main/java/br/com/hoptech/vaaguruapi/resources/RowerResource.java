1db5be11d86f34a0be50fbe1301dbf9262ae34ae
Gustavo Bessa <gbvirtual@gmail.com>
2018-07-03 16:15:29 -0300

    /rowers and /rowers/id endpoints.

---
+++ src/main/java/br/com/hoptech/vaaguruapi/resources/RowerResource.java
@@ -0,0 +1,40 @@
+package br.com.hoptech.vaaguruapi.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.hoptech.vaaguruapi.domain.Rower;
+import br.com.hoptech.vaaguruapi.domain.Team;
+import br.com.hoptech.vaaguruapi.dto.RowerDTO;
+import br.com.hoptech.vaaguruapi.dto.TeamDTO;
+import br.com.hoptech.vaaguruapi.services.RowerService;
+
+@RestController
+@RequestMapping("/rowers")
+public class RowerResource {
+
+    @Autowired
+    RowerService service;
+    
+    @GetMapping()
+    public ResponseEntity<List<RowerDTO>> findAll() {
+	List<Rower> list = service.findAll();
+	List<RowerDTO> listDto = list.stream().map(obj -> new RowerDTO(obj)).collect(Collectors.toList());
+	return ResponseEntity.ok().body(listDto);
+    }
+    
+    @GetMapping(value="/{id}")
+    public ResponseEntity<RowerDTO> find(@PathVariable Integer id) {
+	Rower obj = service.find(id);
+	RowerDTO objDto = new RowerDTO(obj);
+	return ResponseEntity.ok().body(objDto);
+    }
+    
+}
--- src/main/java/br/com/hoptech/vaaguruapi/services/RowerService.java
+++ src/main/java/br/com/hoptech/vaaguruapi/services/RowerService.java
@@ -1,18 +1,30 @@
 package br.com.hoptech.vaaguruapi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.hoptech.vaaguruapi.domain.Rower;
 import br.com.hoptech.vaaguruapi.repositories.RowerRepository;
+import br.com.hoptech.vaaguruapi.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class RowerService {
 
     @Autowired
     RowerRepository repo;
+    
+    public Rower find(Integer id) {
+	Optional<Rower> obj = repo.findById(id);
+	return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Rower.class.getName())); 
+    }
+    
+    public List<Rower> findAll() {
+	return repo.findAll();
+    }
     
     public List<Rower> findByTeam(Integer teamId) {
 	return repo.findByTeams_id(teamId);
