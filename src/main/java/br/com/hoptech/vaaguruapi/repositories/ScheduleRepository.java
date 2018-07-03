b2b736b7f615951f1554ddde0be7fb86988a5bff
Gustavo Bessa <gbvirtual@gmail.com>
2018-07-03 16:32:55 -0300

    Instantiating schedules as inscriptions for initial DB.

--- src/main/java/br/com/hoptech/vaaguruapi/repositories/ScheduleRepository.java
+++ src/main/java/br/com/hoptech/vaaguruapi/repositories/ScheduleRepository.java
@@ -3,9 +3,9 @@ package br.com.hoptech.vaaguruapi.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.hoptech.vaaguruapi.domain.Inscription;
+import br.com.hoptech.vaaguruapi.domain.Schedule;
 
 @Repository
-public interface InscriptionRepository extends JpaRepository<Inscription, Integer>{
+public interface ScheduleRepository extends JpaRepository<Schedule, Integer>{
 
 }
--- src/main/java/br/com/hoptech/vaaguruapi/services/DBService.java
+++ src/main/java/br/com/hoptech/vaaguruapi/services/DBService.java
@@ -2,13 +2,18 @@ package br.com.hoptech.vaaguruapi.services;
 
 import java.text.ParseException;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.hoptech.vaaguruapi.domain.Inscription;
 import br.com.hoptech.vaaguruapi.domain.Rower;
+import br.com.hoptech.vaaguruapi.domain.Schedule;
 import br.com.hoptech.vaaguruapi.domain.Team;
+import br.com.hoptech.vaaguruapi.repositories.InscriptionRepository;
 import br.com.hoptech.vaaguruapi.repositories.RowerRepository;
+import br.com.hoptech.vaaguruapi.repositories.ScheduleRepository;
 import br.com.hoptech.vaaguruapi.repositories.TeamRepository;
 
 @Service
@@ -18,9 +23,14 @@ public class DBService {
     private TeamRepository teamRepository;
     @Autowired
     private RowerRepository rowerRepository;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private InscriptionRepository inscriptionRepository;
 
     public void instantiateDatabase() throws ParseException {
 
+	//CREATING TEAMS AND ROWERS
 	Team team1 = new Team(null, "Fusão VAA", "Equipe do Rodrigão", "");
 	Team team2 = new Team(null, "Mauna Loa - Charitas", "Primeira unidade da Mauna Loa", "");
 	Team team3 = new Team(null, "Mauna Loa - Itaipu", "Segunda unidade da Mauna Loa", "");
@@ -47,6 +57,22 @@ public class DBService {
 	teamRepository.saveAll(Arrays.asList(team1, team2, team3, team4));
 	rowerRepository.saveAll(Arrays.asList(rower1, rower2, rower3, rower4, rower5, rower6));
 	
+	
+	//CREATING SCHEDULE AND INSCRIPTIONS
+	Schedule schedule1 = new Schedule(null, new Date(), 1, 12, team1, "Primeiro treino depois da Copa");
+	Schedule schedule2 = new Schedule(null, new Date(), 1, 24, team4, "Mauna BI-CAMPEÃ");
+	
+	Inscription inscr1 = new Inscription(null, rower5, schedule1, new Date());
+	Inscription inscr2 = new Inscription(null, rower6, schedule1, new Date());
+	Inscription inscr3 = new Inscription(null, rower1, schedule2, new Date());
+	Inscription inscr4 = new Inscription(null, rower2, schedule2, new Date());
+	Inscription inscr5 = new Inscription(null, rower3, schedule2, new Date());
+	
+	schedule1.getInscriptions().addAll(Arrays.asList(inscr1, inscr2));
+	schedule2.getInscriptions().addAll(Arrays.asList(inscr3, inscr4, inscr5));
+	
+	scheduleRepository.saveAll(Arrays.asList(schedule1, schedule2));
+	inscriptionRepository.saveAll(Arrays.asList(inscr1, inscr2, inscr3, inscr4, inscr5));
     }
 
 }
