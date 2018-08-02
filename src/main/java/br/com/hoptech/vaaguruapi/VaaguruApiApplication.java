15c804846275e2bcd139aa3f168c4f6b05895717
Gustavo Bessa <gbvirtual@gmail.com>
2018-08-02 09:10:10 -0300

    Instanciate

--- src/main/java/br/com/hoptech/vaaguruapi/VaaguruApiApplication.java
+++ src/main/java/br/com/hoptech/vaaguruapi/VaaguruApiApplication.java
@@ -1,18 +1,24 @@
 package br.com.hoptech.vaaguruapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.hoptech.vaaguruapi.services.DBService;
+
 @SpringBootApplication
 public class VaaguruApiApplication implements CommandLineRunner {
 
+    	@Autowired
+    	DBService dbService;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(VaaguruApiApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-	    
+	    dbService.instantiateDatabase();
 	}
 }
