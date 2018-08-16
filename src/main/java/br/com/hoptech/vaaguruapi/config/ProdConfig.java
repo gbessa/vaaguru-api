c7bbf15f0fae8087b974ded098495e765446f645
Gustavo Bessa <gbvirtual@gmail.com>
2018-08-16 08:22:39 -0300

    Configing the database instantiation

--- src/main/java/br/com/hoptech/vaaguruapi/config/ProdConfig.java
+++ src/main/java/br/com/hoptech/vaaguruapi/config/ProdConfig.java
@@ -1,15 +1,36 @@
 package br.com.hoptech.vaaguruapi.config;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.hoptech.vaaguruapi.services.DBService;
 import br.com.hoptech.vaaguruapi.services.EmailService;
 import br.com.hoptech.vaaguruapi.services.SmtpEmailService;
 
 @Configuration
 @Profile("prod")
 public class ProdConfig {
+        
+    @Autowired
+    private DBService dbService;
+    
+    @Value("${vaaguru.database.instantiate}")
+    private Boolean instantiateDatabase;
+    
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+	System.out.println(instantiateDatabase);
+	if (instantiateDatabase) {
+	    System.out.println("Entrou");
+	    dbService.instantiateDatabase();
+	}
+	return instantiateDatabase;
+    }
     
     @Bean
     public EmailService emailService() {
